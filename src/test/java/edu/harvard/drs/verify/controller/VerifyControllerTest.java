88831a19255072fff52e05b80cab991b59ffe5f4
William Welling <wwelling@library.tamu.edu>
2021-10-06 17:44:23 -0500

    verify update controller tests

--- src/test/java/edu/harvard/drs/verify/controller/VerifyControllerTest.java
+++ src/test/java/edu/harvard/drs/verify/controller/VerifyControllerTest.java
@@ -18,7 +18,6 @@ package edu.harvard.drs.verify.controller;
 
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.adobe.testing.s3mock.junit5.S3MockExtension;
@@ -96,6 +95,44 @@ public class VerifyControllerTest {
     public void shouldVerifyNotFound() throws Exception {
         String content = "{}";
         this.mockMvc.perform(post("/verify/4265456")
+            .content(content)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shouldVerifyUpdate() throws Exception {
+        String content = "{"
+            + "\"v00001/content/descriptor/400016240_mets.xml\": \"88004448277e0ca3229808bd8fa40327\","
+            + "\"v00001/content/metadata/400016240_structureMap.xml\": \"06328e877392db47a2b59bfa9614470c\","
+            + "\"v00001/content/metadata/400016240_mods.xml\": \"2cffede56db677e4924b24622374ac3b\""
+            + "}";
+        this.mockMvc.perform(post("/verify/100000020/update")
+            .content(content)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldVerifyUpdateConflict() throws Exception {
+        String content = "{"
+            + "\"v00001/content/descriptor/400016240_mets.xml\": \"88004448277e0ca3229808bd8fa403ab\","
+            + "\"v00001/content/data/400016242.doc\": \"f9f645a42c784c2b3d2fe93ccbaf1992\","
+            + "\"v00001/content/metadata/400016240_mods.xml\": \"2cffede56db677e4924b24622374ac3b\""
+            + "}";
+        this.mockMvc.perform(post("/verify/100000020/update")
+            .content(content)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isConflict());
+    }
+
+    @Test
+    public void shouldVerifyUpdateNotFound() throws Exception {
+        String content = "{}";
+        this.mockMvc.perform(post("/verify/4265456/update")
             .content(content)
             .contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON))
