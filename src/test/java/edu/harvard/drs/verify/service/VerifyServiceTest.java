756149ce390c4efb50ff4605315de05bbcba079a
William Welling <wwelling@library.tamu.edu>
2021-11-08 21:19:03 -0600

    only verify against latest version of manifest entries

--- src/test/java/edu/harvard/drs/verify/service/VerifyServiceTest.java
+++ src/test/java/edu/harvard/drs/verify/service/VerifyServiceTest.java
@@ -16,10 +16,12 @@
 
 package edu.harvard.drs.verify.service;
 
+import static edu.harvard.drs.verify.utility.KeyUtility.buildKey;
 import static java.lang.String.valueOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
@@ -27,22 +29,27 @@ import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 import com.adobe.testing.s3mock.junit5.S3MockExtension;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.harvard.drs.verify.AmazonS3TestHelper;
 import edu.harvard.drs.verify.config.AwsConfig;
 import edu.harvard.drs.verify.dto.OcflInventory;
 import edu.harvard.drs.verify.dto.VerificationError;
 import edu.harvard.drs.verify.exception.VerificationException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import software.amazon.awssdk.core.sync.ResponseTransformer;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
 
 /**
@@ -56,6 +63,8 @@ public class VerifyServiceTest {
     private AwsConfig awsConfig;
 
     private VerifyService verifyService;
+
+    private ObjectMapper om;
 
     /**
      * Setup verify service tests.
@@ -73,6 +82,7 @@ public class VerifyServiceTest {
         awsConfig.setBucketName(AmazonS3TestHelper.bucket);
         awsConfig.setEndpointOverride(AmazonS3TestHelper.endpointOverride);
         verifyService = new VerifyService(awsConfig);
+        om = new ObjectMapper();
     }
 
     @AfterAll
@@ -86,10 +96,30 @@ public class VerifyServiceTest {
 
         Map<String, String> input = new HashMap<>() {
             {
-                put("v00001/content/descriptor/400000252_mets.xml", "52fe5cdbf844ebc72fc5d1e10f036279");
-                put("v00001/content/metadata/400000254_textMD.xml", "0aff68fa16c9be40ca946f403e4e5180");
-                put("v00001/content/metadata/400000252_structureMap.xml", "17e0a42b63075f7a60fa1db80cfe26b9");
-                put("v00001/content/data/400000254.txt", "872c1b7d198907a3f3f9e6735b32f0ee");
+                put("descriptor/400000252_mets.xml", "52fe5cdbf844ebc72fc5d1e10f036279");
+                put("metadata/400000254_textMD.xml", "0aff68fa16c9be40ca946f403e4e5180");
+                put("metadata/400000252_structureMap.xml", "17e0a42b63075f7a60fa1db80cfe26b9");
+                put("data/400000254.txt", "872c1b7d198907a3f3f9e6735b32f0ee");
+            }
+        };
+
+        verifyService.verifyIngest(id, input);
+
+        assertTrue(true);
+    }
+
+    @Test
+    public void testVerifyIngestWithMultipleVersions() throws IOException, VerificationException {
+        Long id = 101000305L;
+
+        Map<String, String> input = new HashMap<>() {
+            {
+                put("descriptor/400018804_mets.xml", "485cb52bf9838339b89037b233530e1b");
+                put("metadata/400018804_structureMap.xml", "1684c7f0313b7b1e83e6c6553e236ae1");
+                put("metadata/400018806_mix.xml", "85a1e107b1a6b23ca844cec8e13381f4");
+                put("metadata/400018807_textMD.xml", "d75b3f1329e2b56b53676b32b3dbba99");
+                put("data/400018806.jp2", "5eab6e472a2fc8363104180ac5f4b37a");
+                put("data/400018807.xml", "3a5af24ee7058434a4bcde0dd612fa4a");
             }
         };
 
@@ -104,10 +134,10 @@ public class VerifyServiceTest {
 
         Map<String, String> input = new HashMap<>() {
             {
-                put("v00001/content/descriptor/400000252_mets.xml", "52fe5cdbf844ebc72fc5d1e10f036280");
-                put("v00001/content/metadata/400000252_structureMap.xml", "17e0a42b63075f7a60fa1db80cfe26b9");
-                put("v00001/content/data/400000254.txt", "872c1b7d198907a3f3f9e6735b32f0ee");
-                put("v00001/content/data/9991231.pdf", "32723094875a987b9797dd987ea979712");
+                put("descriptor/400000252_mets.xml", "52fe5cdbf844ebc72fc5d1e10f036280");
+                put("metadata/400000252_structureMap.xml", "17e0a42b63075f7a60fa1db80cfe26b9");
+                put("data/400000254.txt", "872c1b7d198907a3f3f9e6735b32f0ee");
+                put("data/9991231.pdf", "32723094875a987b9797dd987ea979712");
             }
         };
 
@@ -121,20 +151,20 @@ public class VerifyServiceTest {
 
         assertEquals(4, exception.getErrors().size());
 
-        VerificationError checksumError = exception.getErrors().get("v00001/content/descriptor/400000252_mets.xml");
+        VerificationError checksumError = exception.getErrors().get("descriptor/400000252_mets.xml");
         assertNotNull(checksumError);
         assertEquals("Checksums do not match", checksumError.getError());
         assertEquals("52fe5cdbf844ebc72fc5d1e10f036280", checksumError.getExpected());
         assertEquals("52fe5cdbf844ebc72fc5d1e10f036279", checksumError.getActual());
 
-        VerificationError missingError = exception.getErrors().get("v00001/content/metadata/400000254_textMD.xml");
+        VerificationError missingError = exception.getErrors().get("metadata/400000254_textMD.xml");
         assertNotNull(missingError);
         assertEquals("Missing input checksum", missingError.getError());
 
-        VerificationError s3Error = exception.getErrors().get("v00001/content/data/400000254.txt");
+        VerificationError s3Error = exception.getErrors().get("data/400000254.txt");
         assertNotNull(s3Error);
 
-        VerificationError unexpectedError = exception.getErrors().get("v00001/content/data/9991231.pdf");
+        VerificationError unexpectedError = exception.getErrors().get("data/9991231.pdf");
         assertNotNull(unexpectedError);
         assertEquals("Not found in inventory manifest", unexpectedError.getError());
 
@@ -153,8 +183,8 @@ public class VerifyServiceTest {
 
         Map<String, String> input = new HashMap<>() {
             {
-                put("v00001/content/descriptor/400000252_mets.xml", "52fe5cdbf844ebc72fc5d1e10f036279");
-                put("v00001/content/metadata/400000252_structureMap.xml", "17e0a42b63075f7a60fa1db80cfe26b9");
+                put("descriptor/400000252_mets.xml", "52fe5cdbf844ebc72fc5d1e10f036279");
+                put("metadata/400000252_structureMap.xml", "17e0a42b63075f7a60fa1db80cfe26b9");
             }
         };
 
@@ -171,9 +201,9 @@ public class VerifyServiceTest {
 
         Map<String, String> input = new HashMap<>() {
             {
-                put("v00001/content/descriptor/400000252_mets.xml", "52fe5cdbf844ebc72fc5d1e10f036280");
-                put("v00001/content/metadata/400000252_structureMap.xml", "17e0a42b63075f7a60fa1db80cfe26b9");
-                put("v00001/content/data/9991231.pdf", "32723094875a987b9797dd987ea979712");
+                put("descriptor/400000252_mets.xml", "52fe5cdbf844ebc72fc5d1e10f036280");
+                put("metadata/400000252_structureMap.xml", "17e0a42b63075f7a60fa1db80cfe26b9");
+                put("data/9991231.pdf", "32723094875a987b9797dd987ea979712");
             }
         };
 
@@ -185,16 +215,16 @@ public class VerifyServiceTest {
 
         assertEquals(3, exception.getErrors().size());
 
-        VerificationError checksumError = exception.getErrors().get("v00001/content/descriptor/400000252_mets.xml");
+        VerificationError checksumError = exception.getErrors().get("descriptor/400000252_mets.xml");
         assertNotNull(checksumError);
         assertEquals("Checksums do not match", checksumError.getError());
         assertEquals("52fe5cdbf844ebc72fc5d1e10f036280", checksumError.getExpected());
         assertEquals("52fe5cdbf844ebc72fc5d1e10f036279", checksumError.getActual());
 
-        VerificationError s3Error = exception.getErrors().get("v00001/content/metadata/400000252_structureMap.xml");
+        VerificationError s3Error = exception.getErrors().get("metadata/400000252_structureMap.xml");
         assertNotNull(s3Error);
 
-        VerificationError unexpectedError = exception.getErrors().get("v00001/content/data/9991231.pdf");
+        VerificationError unexpectedError = exception.getErrors().get("data/9991231.pdf");
         assertNotNull(unexpectedError);
         assertEquals("Not found in inventory manifest", unexpectedError.getError());
 
@@ -218,6 +248,144 @@ public class VerifyServiceTest {
         OcflInventory inventory = verifyService.getInventory(id);
 
         assertInventory(inventory);
+    }
+
+    @Test
+    public void testReduceManifest(final S3Client s3) throws IOException {
+        Long id = 101000305L;
+
+        String key = buildKey(id, "inventory.json");
+
+        GetObjectRequest request = GetObjectRequest.builder()
+            .bucket(AmazonS3TestHelper.bucket)
+            .key(key)
+            .build();
+
+        try (InputStream is = s3.getObject(request, ResponseTransformer.toInputStream())) {
+            OcflInventory inventory = this.om.readValue(is, OcflInventory.class);
+
+            assertManifestEntryNotNull(
+                inventory,
+                "2ab46874a9030e55ebc23a7e8fdebdffa25cef1ce23cc417aea7e1e53001311e"
+                + "77a26c4c11c021514d32a00c47546ce7be09f906de997808bff7950a2b744da9",
+                "v00001/content/metadata/400018807_textMD.xml"
+            );
+
+            assertManifestEntryNotNull(
+                inventory,
+                "2daf8d458f9cba6fcf46daad06d1d9127e0397cd8773aac021b5da50beb219c5"
+                + "79972cccdc0a6f2e203833fa8778740d0a671ef637b0bd1b37ed9c8f9f2b492d",
+                "v00001/content/data/400018806.jp2"
+            );
+
+            assertManifestEntryNotNull(
+                inventory,
+                "3f86fcda2b23ac0a15b57279a36c0ff185176b3873d67e2e81afe5c559c748f2"
+                + "8795237eaa721a52122c44b23af09e6203ffca27362570a1e351d372f4e2faf5",
+                "v00002/content/descriptor/400018804_mets.xml"
+            );
+
+            assertManifestEntryNotNull(
+                inventory,
+                "4c4e58c54b8fa8a67e2257a7d5e4000fddd9b24ce3b5eb9c5cdb8a51c13bb9cd"
+                + "ab7d489996f48a71c088609b2ce0fba18af4815a2e7838f8d4acf1cdb1612122",
+                "v00001/content/metadata/400018804_structureMap.xml"
+            );
+
+            assertManifestEntryNotNull(
+                inventory,
+                "65494940aebd7d6dab17d3ada273e83e8878bf3403e1b92249a565161139764d"
+                + "1f98ede1b55a4ab140e49bf99518e42b1ee3c9752a32e3dd1b84e2da60c9bd72",
+                "v00004/content/descriptor/400018804_mets.xml"
+            );
+
+            assertManifestEntryNotNull(
+                inventory,
+                "bc6717bc037fb954df066299f73adb56096d308f67158b31bc43983b0f4231e4"
+                + "0cf5e571f639738f4e2c332b6306e3a97455942ebcce0bf2459a8739eb3df08e",
+                "v00003/content/descriptor/400018804_mets.xml"
+            );
+
+            assertManifestEntryNotNull(
+                inventory,
+                "c0ccda2feb51af4dfabba25fcd4828dd7e16ca2ad4f8dc64bd30207cc955015d"
+                + "81b8a0bf9873faa2a7418fa89e9348bc4a38ff419ae0ca546fadcb1764447582",
+                "v00001/content/data/400018807.xml"
+            );
+
+            assertManifestEntryNotNull(
+                inventory,
+                "c91bb2f4ab2f59c607fa9e0b99f793e16cc881c04f22ef37d8406b4f0b50f58a"
+                + "bbe85c033877dfb70f60259d49ffd60f3d1c4254c400d90a8516e85e8d95002e",
+                "v00001/content/descriptor/400018804_mets.xml"
+            );
+
+            assertManifestEntryNotNull(
+                inventory,
+                "ea10c5ce2aea498f1db6878cb99653e963bfe0a010f9570c993bd70b6b64e07e"
+                + "4531d7c30466dfdbe7e6c9c0cb8b3133b8116458e43935ee73822c01bf3768c2",
+                "v00001/content/metadata/400018806_mix.xml"
+            );
+
+            inventory = verifyService.reduceManifest(inventory);
+
+            assertManifestEntryNotNull(
+                inventory,
+                "2ab46874a9030e55ebc23a7e8fdebdffa25cef1ce23cc417aea7e1e53001311e"
+                + "77a26c4c11c021514d32a00c47546ce7be09f906de997808bff7950a2b744da9",
+                "v00001/content/metadata/400018807_textMD.xml"
+            );
+
+            assertManifestEntryNotNull(
+                inventory,
+                "2daf8d458f9cba6fcf46daad06d1d9127e0397cd8773aac021b5da50beb219c5"
+                + "79972cccdc0a6f2e203833fa8778740d0a671ef637b0bd1b37ed9c8f9f2b492d",
+                "v00001/content/data/400018806.jp2"
+            );
+
+            assertNull(inventory.getManifest().get(
+                "3f86fcda2b23ac0a15b57279a36c0ff185176b3873d67e2e81afe5c559c748f2"
+                + "8795237eaa721a52122c44b23af09e6203ffca27362570a1e351d372f4e2faf5"
+            ));
+
+            assertManifestEntryNotNull(
+                inventory,
+                "4c4e58c54b8fa8a67e2257a7d5e4000fddd9b24ce3b5eb9c5cdb8a51c13bb9cd"
+                + "ab7d489996f48a71c088609b2ce0fba18af4815a2e7838f8d4acf1cdb1612122",
+                "v00001/content/metadata/400018804_structureMap.xml"
+            );
+
+            assertManifestEntryNotNull(
+                inventory,
+                "65494940aebd7d6dab17d3ada273e83e8878bf3403e1b92249a565161139764d"
+                + "1f98ede1b55a4ab140e49bf99518e42b1ee3c9752a32e3dd1b84e2da60c9bd72",
+                "v00004/content/descriptor/400018804_mets.xml"
+            );
+
+            assertNull(inventory.getManifest().get(
+                "bc6717bc037fb954df066299f73adb56096d308f67158b31bc43983b0f4231e4"
+                + "0cf5e571f639738f4e2c332b6306e3a97455942ebcce0bf2459a8739eb3df08e"
+            ));
+
+            assertManifestEntryNotNull(
+                inventory,
+                "c0ccda2feb51af4dfabba25fcd4828dd7e16ca2ad4f8dc64bd30207cc955015d"
+                + "81b8a0bf9873faa2a7418fa89e9348bc4a38ff419ae0ca546fadcb1764447582",
+                "v00001/content/data/400018807.xml"
+            );
+
+            assertNull(inventory.getManifest().get(
+                "c91bb2f4ab2f59c607fa9e0b99f793e16cc881c04f22ef37d8406b4f0b50f58a"
+                + "bbe85c033877dfb70f60259d49ffd60f3d1c4254c400d90a8516e85e8d95002e"
+            ));
+
+            assertManifestEntryNotNull(
+                inventory,
+                "ea10c5ce2aea498f1db6878cb99653e963bfe0a010f9570c993bd70b6b64e07e"
+                + "4531d7c30466dfdbe7e6c9c0cb8b3133b8116458e43935ee73822c01bf3768c2",
+                "v00001/content/metadata/400018806_mix.xml"
+            );
+        }
     }
 
     @Test
@@ -313,6 +481,15 @@ public class VerifyServiceTest {
             "fdea3768ba483e0b54c9b6ccedbcedf01443d62a47272819e4715d46b990650f"
             + "5d03827bd116ddc723155332cea220c9a2533ce8427c9e6d3913ee110f478218"
         ).contains("data/400000254.txt"));
+    }
+
+    private void assertManifestEntryNotNull(OcflInventory inventory, String sha512, String key) {
+        assertNotNull(inventory.getManifest().get(sha512));
+
+        assertEquals(
+            inventory.getManifest().get(sha512),
+            Arrays.asList(new String[] { key })
+        );
     }
 
 }
